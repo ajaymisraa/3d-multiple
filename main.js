ce1f752a40edf7d4cb8e635881fcfe72e08f21cc
Ajay Misra <60949776+ajaymisraa@users.noreply.github.com>
2023-12-03 12:37:08 -0500

    Update main.js
    revert

--- main.js
+++ main.js
@@ -82,17 +82,16 @@ if (new URLSearchParams(window.location.search).get("clear")) {
         windowManager = new WindowManager();
         windowManager.setWinShapeChangeCallback(updateWindowShape);
         windowManager.setWinChangeCallback(windowsUpdated);
-    
+
         // Increment shapeCounter each time a new window is initialized
         shapeCounter = (shapeCounter + 1) % 3;
-        console.log("Current Shape Counter: ", shapeCounter); // Debugging line
-    
+
         // here you can add your custom metadata to each windows instance
         let metaData = {foo: "bar"};
-    
+
         // this will init the windowmanager and add this window to the centralised pool of windows
         windowManager.init(metaData);
-    
+
         // call update windows initially (it will later be called by the win change callback)
         windowsUpdated();
     }
@@ -103,39 +102,39 @@ if (new URLSearchParams(window.location.search).get("clear")) {
 
     function updateNumberOfCubes() {
         let wins = windowManager.getWindows();
-    
-        // remove all previous shapes
+
+        // remove all cubes
         cubes.forEach((c) => {
             world.remove(c);
         });
-    
+
         cubes = [];
-    
-        // add new shapes based on the current window setup and shapeCounter
+
+        // add new shapes based on the current window setup
         for (let i = 0; i < wins.length; i++) {
             let win = wins[i];
-    
+
             let c = new t.Color();
             c.setHSL(i * .1, 1.0, .5);
-    
+
             let s = 100 + i * 50;
             let geometry;
             switch (shapeCounter) {
                 case 0: // Cube
                     geometry = new t.BoxGeometry(s, s, s);
                     break;
-                case 1: // Triangle (using a pyramid to represent)
-                    geometry = new t.ConeGeometry(s, s, 4); // 4-sided pyramid
+                case 1: // Triangle
+                    geometry = new t.ConeGeometry(s, s, 3); // Triangle as a 3-sided cone
                     break;
-                case 2: // Sphere
-                    geometry = new t.SphereGeometry(s, 32, 32); // Sphere geometry
+                case 2: // Circle
+                    geometry = new t.CircleGeometry(s, 32); // Circle geometry
                     break;
             }
-    
+
             let shape = new t.Mesh(geometry, new t.MeshBasicMaterial({ color: c, wireframe: true }));
             shape.position.x = win.shape.x + (win.shape.w * .5);
             shape.position.y = win.shape.y + (win.shape.h * .5);
-    
+
             world.add(shape);
             cubes.push(shape);
         }
@@ -149,44 +148,37 @@ if (new URLSearchParams(window.location.search).get("clear")) {
 
     function render() {
         let t = getTime();
-    
+
         windowManager.update();
-    
+
         // calculate the new position based on the delta between current offset and new offset times a falloff value (to create the nice smoothing effect)
         let falloff = .05;
         sceneOffset.x = sceneOffset.x + ((sceneOffsetTarget.x - sceneOffset.x) * falloff);
         sceneOffset.y = sceneOffset.y + ((sceneOffsetTarget.y - sceneOffset.y) * falloff);
-    
+
         // set the world position to the offset
         world.position.x = sceneOffset.x;
         world.position.y = sceneOffset.y;
-    
+
         let wins = windowManager.getWindows();
-    
+
         // loop through all our cubes and update their positions based on current window positions
         for (let i = 0; i < cubes.length; i++) {
             let cube = cubes[i];
             let win = wins[i];
-    
-            if (!win || !win.shape) {
-                console.error(`Missing or invalid window data at index ${i}.`);
-                continue; // Skip this iteration if the window data is not valid
-            }
-    
-            let _t = t; // Adjust time offset if necessary
-    
+            let _t = t;// + i * .2;
+
             let posTarget = {x: win.shape.x + (win.shape.w * .5), y: win.shape.y + (win.shape.h * .5)}
-    
+
             cube.position.x = cube.position.x + (posTarget.x - cube.position.x) * falloff;
             cube.position.y = cube.position.y + (posTarget.y - cube.position.y) * falloff;
             cube.rotation.x = _t * .5;
             cube.rotation.y = _t * .3;
         };
-    
+
         renderer.render(scene, camera);
         requestAnimationFrame(render);
     }
-
 
     // resize the renderer to fit the window size
     function resize() {
